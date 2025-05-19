a8547deb390c3d7aeaf0e7bb553610a01b5e2cc7
Thisisnotbrai <157675533+Thisisnotbrai@users.noreply.github.com>
2025-05-20 00:33:34 +0800

    Supabase connection

---
+++ src/supabase-client.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string;
+
+export const supabase = createClient(supabaseURL, supabaseAnonKey);
