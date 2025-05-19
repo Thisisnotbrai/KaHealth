1ec02988c250cb5399fa2a8c81496b01bfc9b47f
Thisisnotbrai <157675533+Thisisnotbrai@users.noreply.github.com>
2025-05-20 01:00:53 +0800

    minor changes - deleted tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
+import React from 'react'
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+    <div>
+ 
     </div>
   )
 }
