a74126780d2f9ab2e07fed700a46afbd268da9fe
Thisisnotbrai <157675533+Thisisnotbrai@users.noreply.github.com>
2025-05-27 02:24:02 +0800

    Navbar and Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { ModeToggle } from "./components/ui/mode-toggle"
+import { Navbar5 } from "./components/ui/Navbar"
+import { ThemeProvider } from "./components/ui/theme-provider"
 
 function App() {
   return (
     <div>
- 
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+    </ThemeProvider>
+     <Navbar5 />
     </div>
   )
 }
