00a74271bf2e00df62fe562edda331c03182e3a8
Thisisnotbrai <157675533+Thisisnotbrai@users.noreply.github.com>
2025-08-05 03:08:01 +0800

    Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { ThemeProvider } from "./components/ui/Darkmode/theme-provider"
-import LandingPage from "./components/ui/LandingPage"
+import { ThemeProvider } from "./components/ui/Darkmode/theme-provider";
+import LandingPage from "./components/ui/LandingPage";
+import IntroAnimation from "./components/ui/IntroAnimation";
 
 function App() {
   return (
     <div>
+      <IntroAnimation />
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <LandingPage />
+        <LandingPage />
       </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
