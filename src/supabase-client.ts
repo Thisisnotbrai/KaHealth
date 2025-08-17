af17a27d25f3b5f30b8e905da8389fe2c350935e
Thisisnotbrai <157675533+Thisisnotbrai@users.noreply.github.com>
2025-08-18 02:48:06 +0800

    supabase client export

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -4,3 +4,5 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string;
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
+
+export type { SupabaseClient } from "@supabase/supabase-js";
