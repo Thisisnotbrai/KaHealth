6b118be4c7dd8205ecb540ef99c3f131d7cc6176
Thisisnotbrai <157675533+Thisisnotbrai@users.noreply.github.com>
2025-08-18 02:48:59 +0800

    supabase revert changes

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -4,5 +4,3 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string;
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
-
-export type { SupabaseClient } from "@supabase/supabase-js";
